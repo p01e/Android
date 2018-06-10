8772231e4cae562d9ce48f10c0f988a9fdd4ee87
p01e <40141898+p01e@users.noreply.github.com>
2018-06-10 19:23:23 +0530

    Add files via upload

---
+++ 2-Fragment (20,21-04-2018)/java/tops/com/barberapp/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package tops.com.barberapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tops.com.barberapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
